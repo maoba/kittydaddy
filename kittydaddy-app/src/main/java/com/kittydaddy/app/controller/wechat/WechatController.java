0bf0359b0e7a5f35c9f2ac6f5721319ff10e73e4
lujianhao <lujianhao@shanlincaifu.com>
2018-02-01 10:19:44 +0800

    微信连接相关代码提交

--- kittydaddy-app/src/main/java/com/kittydaddy/app/controller/wechat/WechatController.java
+++ kittydaddy-app/src/main/java/com/kittydaddy/app/controller/wechat/WechatController.java
@@ -32,7 +32,7 @@ public class WechatController extends BaseController{
 	
 	 /**
      * 微信接入
-     * @param wc
+     * @param 
      * @return
      * @throws IOException 
      */
@@ -40,25 +40,40 @@ public class WechatController extends BaseController{
     @ResponseBody
     public void connectWeixin(HttpServletRequest request, HttpServletResponse response) throws IOException{
         request.setCharacterEncoding("UTF-8");  //微信服务器POST消息时用的是UTF-8编码，在接收时也要用同样的编码，否则中文会乱码；
-        response.setCharacterEncoding("UTF-8"); //在响应消息（回复消息给用户）时，也将编码方式设置为UTF-8，原理同上；boolean isGet = request.getMethod().toLowerCase().equals("get"); 
+        response.setCharacterEncoding("UTF-8"); //在响应消息（回复消息给用户）时，也将编码方式设置为UTF-8，原理同上；
+        boolean isGet = request.getMethod().toLowerCase().equals("get"); 
       
         PrintWriter out = response.getWriter();
-           try {
-                String signature = request.getParameter("signature");// 微信加密签名  
-                String timestamp = request.getParameter("timestamp");// 时间戳  
-                String nonce = request.getParameter("nonce");// 随机数  
-                String echostr = request.getParameter("echostr");//随机字符串  
-                // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败 
-                if(KStringUtils.isNotEmpty(echostr)&&KSignUtil.checkSignature(token, signature, timestamp, nonce)){
-                	LOGGER.info("Connect the weixin server is successful.");
-                    out.write(echostr);
-                }else{
-                	wechatService.weixinPost(request);
-                } 
-	        } catch (Exception e) {
-	            LOGGER.error("Connect the weixin server is error.");
-	        }finally{
-	            out.close();
-	        }
-        }
+        try {
+            if (isGet) {
+              String signature = request.getParameter("signature");// 微信加密签名
+              String timestamp = request.getParameter("timestamp");// 时间戳
+              String nonce = request.getParameter("nonce");// 随机数
+              String echostr = request.getParameter("echostr");//随机字符串
+             
+              // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+            if (KSignUtil.checkSignature(token, signature, timestamp, nonce)) {
+                  LOGGER.info("Connect the weixin server is successful.");
+                  response.getWriter().write(echostr);
+               } else {
+                 LOGGER.error("Failed to verify the signature!");
+               }
+            }else{
+                 String respMessage = "异常消息！";
+              try {
+                 respMessage = wechatService.weixinPost(request);
+                 out.write(respMessage);
+                 LOGGER.info("The request completed successfully");
+                 LOGGER.info("to weixin server "+respMessage);
+              } catch (Exception e) {
+                 LOGGER.error("Failed to convert the message from weixin!");
+              }
+             
+            }
+          } catch (Exception e) {
+               LOGGER.error("Connect the weixin server is error.");
+          }finally{
+              out.close();
+          }
+      }
  }
